1f74b8bc52e20b35f79c859180f8f60f5afa93c1
GitHub Action <action@github.com>
2026-05-21 12:42:22 +0000

    Bump version to 2.11.28.fix (211028)

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ subprojects {
             minSdk = 21
             targetSdk = 35
 
-            versionName = "2.11.28"
+            versionName = "2.11.28.fix"
             versionCode = 211028
 
             resValue("string", "release_name", "v$versionName")
